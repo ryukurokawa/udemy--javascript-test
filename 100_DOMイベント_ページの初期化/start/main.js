45990d4b9463585328230dcb31c6dff4f26ddae3
りゅう <setsuna1228@gmail.com>
2025-11-15 20:24:49 +0900

    講座受講中

--- 100_DOMイベント_ページの初期化/start/main.js
+++ 100_DOMイベント_ページの初期化/start/main.js
@@ -1,10 +1,12 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+document.addEventListener('DOMContentLoaded', function () {
+  const dcl = document.querySelector('.dcl');
+  dcl.classList.add('done');
 
-document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
+  const h1 = document.querySelector('h1');
+  h1.style.color = 'red';
 });
 
-window.addEventListener("load", function () {
-    load.classList.add('done');
+window.addEventListener('load', function () {
+  const load = document.querySelector('.load');
+  load.classList.add('done');
 });
--- 115_thisとbind/start/main.js
+++ 115_thisとbind/start/main.js
@@ -1,13 +1,16 @@
 const obj = {
-    first_name: 'Mafia',
-    last_name: 'Code',
-    printFullName: function () {
-        console.log(this);
+  first_name: 'Mafia',
+  last_name: 'Code',
+  printFullName: function () {
+    console.log(this);
+    const _that = this;
 
-        window.setTimeout(function () {
-            console.log(this);
-        });
-    }
-}
+    window.setTimeout(
+      function () {
+        console.log(this);
+      }.bind({ first_name: 'Taro' })
+    );
+  },
+};
 
 obj.printFullName();
